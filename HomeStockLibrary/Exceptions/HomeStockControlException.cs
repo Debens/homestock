081fcd6cb18acb0633a53a72bc3dce8b7250cc81
Andrew <A.Debens@hotmail.com>
2017-03-06 21:00:06 +0000

    Creating HomeStock exceptions

---
+++ HomeStockLibrary/Exceptions/HomeStockControlException.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeStockLibrary.Exceptions
+{
+    public class HomeStockControlException : Exception
+    {
+        public HomeStockControlException()
+        {
+        }
+
+        public HomeStockControlException(string message) : base(message)
+        {
+
+        }
+
+        public HomeStockControlException(string message, Exception inner) : base(message, inner)
+        { 
+
+        }
+    }
+}
---
+++ HomeStockLibrary/Exceptions/HomeStockException.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeStockLibrary.Exceptions
+{
+    public class HomeStockException : Exception
+    {
+        public HomeStockException()
+        {
+        }
+
+        public HomeStockException(string message) : base(message)
+        {
+
+        }
+
+        public HomeStockException(string message, Exception inner) : base(message, inner)
+        {
+
+        }
+    }
+}
