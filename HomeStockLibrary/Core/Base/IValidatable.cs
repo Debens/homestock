7a0be5723d33ccbe23ce0106bdc58f1b167943e8
Andrew <A.Debens@hotmail.com>
2017-03-06 21:01:30 +0000

    Adding validatable interface for validation control properties

---
+++ HomeStockLibrary/Core/Base/IValidatable.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeStockLibrary.Core.Base
+{
+    public interface IValidatable
+    {
+        void ValidateProperties();
+
+        void Validate(string value, string propertyName);
+
+        void Validate(string value, string propertyName, string message);
+    }
+}
---
+++ HomeStockLibrary/Core/HomeStockScriptObject.cs
@@ -0,0 +1,25 @@
+﻿using HomeStockLibrary.Controls.Base;
+using HomeStockLibrary.Exceptions;
+using HomeStockLibrary.Util.Script;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.UI;
+
+namespace HomeStockLibrary.Core
+{
+    public abstract class HomeStockScriptObject : HomeStockControlBase
+    {
+        public abstract string GenerateCreationString();
+
+        protected override void Render(HtmlTextWriter writer)
+        {
+            string creationString = GenerateCreationString();
+            Validate(creationString, new HomeStockControlException(string.Format("Control '{0}', of type {1}, GenerateCreationString returned an invalid result", this.ID, this.GetType())));
+
+            HomeStockScriptAssistant.RenderScript(new HomeStockScript(creationString));
+        }
+    }
+}
