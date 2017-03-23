745c2af5147faa15a245edf4dcf413d5927b9348
Andrew Debens <adebens@customersystems.com>
2017-03-23 16:27:57 +0000

    UUID changes/fix

--- Homestock.Business/0-Utility/UUID.js
+++ Homestock.Business/0-Utility/UUID.js
@@ -10,11 +10,11 @@
         if (typeof performance !== 'undefined' && typeof performance.now === 'function') {
             d += performance.now(); //use high-precision timer if available
         }
-        return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
+        var uuid = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
             var r = (d + Math.random() * 16) % 16 | 0;
             d = Math.floor(d / 16);
-            var uuid = (c === 'x' ? r : (r & 0x3 | 0x8)).toString(16);
-            return prefix ? prefix.toString() + "-" + uuid : uuid;
+            return (c === 'x' ? r : (r & 0x3 | 0x8)).toString(16);
         });
+        return prefix ? prefix.toString() + "-" + uuid : uuid;
     }
 })();
