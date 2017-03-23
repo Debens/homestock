71d713e644d3b2d7833cf126c31ea42027d71267
Andrew Debens <adebens@customersystems.com>
2017-03-23 16:27:47 +0000

    Allowing prefixes to UUID's, this will allow you to tell what a record is by its ID, e.g. 'USR-fe378fb0-2876-41cd-a9fd-311fbd16d364' or 'STK-856efcf4-4a27-449e-b9a6-1ed8f798cb0a'

--- Homestock.Business/0-Utility/UUID.js
+++ Homestock.Business/0-Utility/UUID.js
@@ -5,7 +5,7 @@
     var ns = HomeStock.Import(nsString);
     var messagePrefix = nsString + ".UUID: ";
 
-    ns.UUID = function () { // Public Domain/MIT
+    ns.UUID = function (prefix) { // Public Domain/MIT
         var d = new Date().getTime();
         if (typeof performance !== 'undefined' && typeof performance.now === 'function') {
             d += performance.now(); //use high-precision timer if available
@@ -13,7 +13,8 @@
         return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
             var r = (d + Math.random() * 16) % 16 | 0;
             d = Math.floor(d / 16);
-            return (c === 'x' ? r : (r & 0x3 | 0x8)).toString(16);
+            var uuid = (c === 'x' ? r : (r & 0x3 | 0x8)).toString(16);
+            return prefix ? prefix.toString() + "-" + uuid : uuid;
         });
     }
 })();
