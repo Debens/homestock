43c864afeb5e97ee382bbb17312d2c1fafb6c2cc
Andrew Debens <adebens@customersystems.com>
2017-03-23 16:18:00 +0000

    Adding UUID generator

---
+++ Homestock.Business/0-Utility/UUID.js
@@ -0,0 +1,19 @@
+﻿; (function () {
+    "use strict";
+
+    var nsString = "Util";
+    var ns = HomeStock.Import(nsString);
+    var messagePrefix = nsString + ".UUID: ";
+
+    ns.UUID = function () { // Public Domain/MIT
+        var d = new Date().getTime();
+        if (typeof performance !== 'undefined' && typeof performance.now === 'function') {
+            d += performance.now(); //use high-precision timer if available
+        }
+        return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
+            var r = (d + Math.random() * 16) % 16 | 0;
+            d = Math.floor(d / 16);
+            return (c === 'x' ? r : (r & 0x3 | 0x8)).toString(16);
+        });
+    }
+})();
