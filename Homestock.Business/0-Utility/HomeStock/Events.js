a1ba92fccd438d2cf7ca05976e5a1ba6de225b2e
Andrew Debens <a.debens@hotmail.com>
2017-03-26 16:00:50 +0100

    Adding wrapper aroung JQuery event objects such that they can be extended later.

---
+++ Homestock.Business/0-Utility/HomeStock/Events.js
@@ -0,0 +1,30 @@
+﻿; (function () {
+    "use strict";
+
+    var nsString = "HomeStock";
+    var messagePrefix = nsString + ".Events: ";
+
+    HomeStock.EventObj = function () {
+        var self = this;
+
+        var _eventObj = $({});
+
+        self.eventObj = function () { return _eventObj; };
+
+        self.On = function (eventName, callback) {
+            if (eventName) {
+                if (!callback || typeof callback !== "function")
+                    throw messagePrefix + "Callback function is required to create trigger";
+                    _eventObj.on(eventName, callback);
+                }
+        };
+
+        self.Off = function (eventName) {
+            if (eventName) { _eventObj.off(eventName); }
+        };
+
+        self.Trigger = function (eventName, triggerArguments) {
+            if (eventName) { _eventObj.trigger(eventName, triggerArguments); }
+        };
+    };
+})();
--- Homestock.Business/Data/Suppliers/Core/Supplier.js
+++ Homestock.Business/Data/Suppliers/Core/Supplier.js
@@ -12,18 +12,43 @@
         "schemaId"
     ];
 
-    function Supplier(params) {
+    function Supplier(params, protectedData) {
         params = params || {};
         var validation = ObjectValidator.Validate(params, requiredParams);
         if (!validation.isValid)
             throw messagePrefix + "Failed construction, missing parameter(s) " + validation.missingProperties.join(", ") + ", for supplier '" + params.id + "'";
         var self = this;
 
+        protectedData = protectedData || {};
+        protectedData.Read = function () { throw messagePrefix + "Read Not Implemented"; };
+        protectedData.Save = function () { throw messagePrefix + "Save Not Implemented"; };
+        protectedData.Delete = function () { throw messagePrefix + "Delete Not Implemented"; };
+
+        var eventObj = new HomeStock.EventObj();
+
         self.Name = params.id;
 
-        self.Read = function () { throw messagePrefix + "Read Not Implemented"; };
-        self.Save = function () { throw messagePrefix + "Save Not Implemented"; };
-        self.Delete = function () { throw messagePrefix + "Delete Not Implemented"; };
+        self.Read = function (params) {
+            eventObj.Trigger("PreRead", [self]);
+
+            protectedData.Read(params);
+
+            eventObj.Trigger("Read");
+        };
+        self.Save = function (recordSet) {
+            eventObj.Trigger("PreSave", [recordSet, self]);
+
+            protectedData.Save(recordSet);
+            
+            eventObj.Trigger("Save");
+        };
+        self.Delete = function (recordSet) {
+            eventObj.Trigger("PreDelete", [recordSet, self]);
+
+            protectedData.Delete(params);
+
+            eventObj.Trigger("Delete");
+        };
 
         return self;
     };
