880ce43ee5dd8c4780bc9a74d535542af4828ff8
Andrew Debens <adebens@customersystems.com>
2017-03-30 09:13:41 +0100

    Adding Schema and API supplier markup to the Site.Master

--- HomeStock/Development/Library/Data/Suppliers.aspx.designer.cs
+++ HomeStock/Development/Library/Data/Suppliers.aspx.designer.cs
@@ -19,7 +19,7 @@ namespace HomeStock.Development.Library.Data {
         /// Auto-generated field.
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
-        protected global::HomeStockLibrary.Data.Suppliers.APISupplier apiSupplier;
+        protected global::HomeStockLibrary.Data.Suppliers.API apiSupplier;
         
         /// <summary>
         /// ASchemaID control.
--- HomeStock/Site.Master.designer.cs
+++ HomeStock/Site.Master.designer.cs
@@ -29,5 +29,23 @@ namespace HomeStock {
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::HomeStockLibrary.Controls.HomeStockScriptControl SiteMasterScriptAssistant;
+        
+        /// <summary>
+        /// apiHomeStock control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::HomeStockLibrary.Data.Suppliers.APISupplier apiHomeStock;
+        
+        /// <summary>
+        /// homeStockSchema control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::HomeStockLibrary.Data.Schemas.Schema homeStockSchema;
     }
 }
