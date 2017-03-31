f33dbc4b347265033e455194fde680eb007984ec
Andrew Debens <adebens@customersystems.com>
2017-03-31 16:04:18 +0100

    Adding HomeStock logging wrapper

---
+++ Homestock.Business/0-Utility/HomeStock/Logging.js
@@ -0,0 +1,22 @@
+﻿; (function () {
+    "use strict";
+
+    var nsString = "HomeStock";
+    var messagePrefix = nsString + ".Logging: ";
+
+    HomeStock.Log = function (log) {
+        console.log(log)
+    };
+    HomeStock.Info = function (info) {
+        console.info(info)
+    };
+    HomeStock.Debug = function (debug) {
+        console.debug(debug)
+    };
+    HomeStock.Warn = function (warning) {
+        console.warn(warning)
+    };
+    HomeStock.Error = function (error) {
+        console.error(error)
+    };
+})();
