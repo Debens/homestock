040937b885a0706f757c61363159ddfecf76a546
Andrew Debens <adebens@customersystems.com>
2017-03-31 17:31:28 +0100

    Small changes

--- HomeStockLibrary/Util/HomeStockObjectAssistant.cs
+++ HomeStockLibrary/Util/HomeStockObjectAssistant.cs
@@ -12,26 +12,26 @@ namespace HomeStockLibrary.Util
     {
         public static IEnumerable<PropertyInfo> GetPropertyInfoRecursively(Type type)
         {
-            var properties = type.GetProperties();
-
-            foreach (var property in properties)
+            var properties = type.GetProperties(BindingFlags.FlattenHierarchy | BindingFlags.Public | BindingFlags.Instance);
+            foreach (var propertyInfo in properties)
             {
-                var genericType = GetGenericArguemnt(property.PropertyType);
-                //if (typeof(IList).IsAssignableFrom(property.PropertyType))
-                //{
-                    yield return property;
+                if (typeof(IList).IsAssignableFrom(propertyInfo.PropertyType))
+                {
+                    var genericType = propertyInfo.PropertyType.GetGenericArguments()[0];
+                    yield return propertyInfo;
                     foreach (var info in GetPropertyInfoRecursively(genericType))
-                         yield return info;
-                //}
-                //else if (property.PropertyType.Module.ScopeName != "CommonLanguageRuntimeLibrary")
-                //{
-                //    foreach (var info in GetPropertyInfoRecursively(genericType))
-                //        yield return info;
-                //}
-                //else
-                //{
-                //    yield return property;
-                //}
+                        yield return info;
+                }
+                else if (propertyInfo.PropertyType.Module.ScopeName != "CommonLanguageRuntimeLibrary")
+                {
+                    yield return propertyInfo;
+                    foreach (var info in GetPropertyInfoRecursively(propertyInfo.PropertyType))
+                        yield return info;
+                }
+                else
+                {
+                    yield return propertyInfo;
+                }
             }
 
             var interfaces = type.GetInterfaces();
@@ -40,15 +40,6 @@ namespace HomeStockLibrary.Util
                 foreach (var info in GetPropertyInfoRecursively(subInterface))
                     yield return info;
             }
-        }
-
-        public static Type GetGenericArguemnt(Type type)
-        {
-            if (!type.IsGenericType)
-                return type;
-            if (type.GetGenericTypeDefinition() != typeof(List<>))
-                throw new Exception("Only List<T> are allowed");
-            return type.GetGenericArguments()[0];
         }
     }
 }
