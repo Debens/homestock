5cc02c3d5989e10c556a20d8573f4d7f66c1ebbc
Andrew Debens <adebens@customersystems.com>
2017-04-04 16:14:14 +0100

    Small formatting

--- Homestock.Business/0-Utility/HomeStock/Logging.js
+++ Homestock.Business/0-Utility/HomeStock/Logging.js
@@ -24,7 +24,7 @@
 
     function Write(message, type) {
         if (nsRoot.Logging.Enabled())
-            if ((!!message && typeof message === typeof "string") && typeof console[type] === "function")
+            if (message && typeof message === typeof "string" && typeof console[type] === "function")
                 console[type](message);
-    }
+    };
 })();
