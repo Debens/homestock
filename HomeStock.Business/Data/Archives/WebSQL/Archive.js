9913301ce931acd13694ac20719f5a16dcbe6d7c
Andrew Debens <adebens@customersystems.com>
2017-04-10 11:00:44 +0100

    Adding table builder to build all WebSQL archive tables from a schema

--- HomeStock.Business/Data/Archives/WebSQL/Archive.js
+++ HomeStock.Business/Data/Archives/WebSQL/Archive.js
@@ -3,6 +3,7 @@
 
     var nsString = "Data.Archives.WebSQL", ns = HomeStock.Import(nsString);
     var nsCoreString = "Data.Archives.Core", nsCore = HomeStock.Import(nsCoreString);
+    var nsSQLString = "Util.SQL", nsSQL = HomeStock.Import(nsSQLString);
 
     ns.Export("Archive", Archive);
     var messagePrefix = nsString + ".Archive: ";
@@ -10,6 +11,13 @@
     function Archive(params) {
         this.validate(params, "tableName");
         var self = new nsCore.Archive(params);
+
+        initTables(self.Schema());
+
+        function initTables (schema) {
+            schema = schema || self.Schema();
+            new nsSQL.TableBuilder().Build(schema);
+        };
 
         return self;
     };
---
+++ Homestock.Business/0-Utility/Sql/TableBuilder.js
@@ -0,0 +1,36 @@
+﻿; (function () {
+    "use strict";
+
+    var nsString = "Util.SQL", ns = HomeStock.Import(nsString);
+
+    ns.Export("TableBuilder", TableBuilder);
+    var messagePrefix = nsString + ".TableBuilder: ";
+
+    function TableBuilder (params) {
+        this.validate(params, "schemaId");
+        var self = this;
+
+        var schemaId = params.schemaId;
+        var schema = function () { return HomeStock.Schemas[schemaId]; };
+        
+        self.Build = function () {
+            var tableNames = Object.keys(schema().Entities);
+
+            for (var index = 0; index < tableNames.length; index++) {
+                var tableName = tableNames[index];
+                var columnNames = Object.keys(schema().Entities[tableName].Columns);
+                buildTable(tableName, columnNames);
+            }
+        };
+
+        function buildTable(tableName, columns) {
+            if (!tableName)
+                return;
+            this.validate.isArray(columns)
+
+            var transaction = new ns.Transaction();
+            //TODO: Set the ID column if there is one.
+            transaction.CreateTable(tableName, columns).Uniquely().Run();
+        };
+    };
+})();
