89b5615aef42515bdecf4ad047eec2349456280a
Andrew Debens <adebens@customersystems.com>
2017-04-11 12:27:00 +0100

    Removing _old, it is now completely outdated

--- Homestock.Business/_old/mark i/Manager/HomeManager.js
+++
@@ -1,7 +0,0 @@
-; (function() {
-	"use strict";
-
-	window.HomeManger = function() {
-		var self = this;
-	}
-})();
--- Homestock.Business/_old/mark i/Manager/OwnerManager.js
+++
@@ -1,7 +0,0 @@
-; (function() {
-	"use strict";
-
-	window.OwnerManager = function() {
-		var self = this;
-	}
-})();
