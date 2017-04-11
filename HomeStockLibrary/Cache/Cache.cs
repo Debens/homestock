18f651fc9f8de1e3dcd1d1e02022a1b2f5094b20
Andrew Debens <adebens@customersystems.com>
2017-04-11 16:31:18 +0100

    Adding caching classes

---
+++ HomeStockLibrary/Cache/Cache.cs
@@ -0,0 +1,45 @@
+﻿using HomeStockLibrary.Exceptions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeStockLibrary.Cache
+{
+    public class Cache<TKey, TValue>
+    {
+        protected Dictionary<TKey, TValue> cache { get; set; }
+
+        public Cache()
+        {
+            cache = new Dictionary<TKey, TValue>();
+        }
+
+        public virtual TValue this[TKey key]
+        {
+            get
+            {
+                if (HasCached(key))
+                    return cache[key];
+                else
+                    throw new KeyNotFoundException("Cache does not define a value under the given key");
+            }
+            set
+            {
+                cache.Add(key, value);
+            }
+        }
+
+        public virtual void Clear()
+        {
+            cache.Clear();
+        }
+
+        public virtual bool HasCached(TKey key)
+        {
+            TValue value;
+            return cache.TryGetValue(key, out value);
+        }
+    }
+}
---
+++ HomeStockLibrary/Cache/SmartCache.cs
@@ -0,0 +1,34 @@
+﻿using HomeStockLibrary.Exceptions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeStockLibrary.Cache
+{
+    public class SmartCache<TKey, TValue> : Cache<TKey, TValue>
+    {
+        private Func<TKey, TValue> accessor { get; set; }
+
+        public SmartCache(Func<TKey, TValue> accessor) : base() {
+            this.accessor = accessor;
+        }
+
+        public override TValue this[TKey key] {
+            get {
+                if (HasCached(key))
+                    return cache[key];
+                else
+                {
+                    TValue value = accessor(key);
+                    if (value == null)
+                        throw new HomeStockException("Cache accessor function failed to return a value");
+
+                    cache.Add(key, value);
+                    return value;
+                }
+            } 
+        }
+    }
+}
