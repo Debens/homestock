c2df55add5dc02f434fe9351820e1df497afdfb0
Andrew Debens <adebens@customersystems.com>
2017-04-12 17:21:20 +0100

    Adding Accordion List library control and template

---
+++ HomeStockLibrary/Controls/AccordionList.cs
@@ -0,0 +1,25 @@
+﻿using HomeStockLibrary.Controls.Base;
+using HomeStockLibrary.Controls.Templates.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.UI;
+
+namespace HomeStockLibrary.Controls
+{
+    public class AccordionList : WebControlBase
+    {
+        protected override void Render(HtmlTextWriter writer)
+        {
+            Templator pageHeader = Templator.Load("AccordionList.html").Process(this);
+            writer.Write(pageHeader.ToString());
+        }
+
+        public override void ValidateProperties()
+        {
+            //Nothing yet.
+        }
+    }
+}
