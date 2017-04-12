ce5d57b747f895e6d38a3054bc25656355be7a23
Andrew Debens <adebens@customersystems.com>
2017-04-12 17:21:37 +0100

    Adding knockout bindings event handlers

---
+++ Homestock.Business/UI/Knockout/Bindings/Events/ToggleClass.js
@@ -0,0 +1,33 @@
+﻿; (function () {
+    if (!ko)
+        return;
+
+    ko.bindingHandlers.toggleClass = {
+        init: function (el, valueAccessor, allBindings) {
+            var classes = format(ko.unwrap(valueAccessor()));
+
+            var actionElement = $(el);
+            var parentSelect = allBindings.get("sParent");
+            var childSelect = allBindings.get("sChild");
+            var event = allBindings.get("event") || "click";
+
+            $(el).on(event, function () {
+                var actionElement = $(el);
+                actionElement = parentSelect ? actionElement.closest(parentSelect.startsWith(".") ? parentSelect : "." + parentSelect) : actionElement;
+                actionElement = childSelect ? actionElement.find(childSelect.startsWith(".") ? childSelect : "." + childSelect) : actionElement;
+
+                var enabled = actionElement.hasClass(classes.on);
+                actionElement.toggleClass(classes.on, !enabled);
+                if (classes.off)
+                    actionElement.toggleClass(classes.off, enabled);
+            });
+        }
+    };
+
+    function format(toggleClasses) {
+        if (typeof toggleClasses === "string")
+            return { on: toggleClasses };
+        else
+            return toggleClasses;
+    };
+})();
---
+++ Homestock.Business/UI/Knockout/Bindings/Events/ToggleExpand.js
@@ -0,0 +1,38 @@
+﻿; (function () {
+    if (!ko)
+        return;
+
+    var defaultOptions = {
+        duration: 500,
+        easing: "linear"
+    };
+
+    ko.bindingHandlers.toggleExpand = {
+        init: function (el, valueAccessor, allBindings) {
+            var intialState = ko.unwrap(valueAccessor());
+
+            var actionElement = $(el);
+            var parentSelect = allBindings.get("sParent");
+            var childSelect = allBindings.get("sChild");
+            var options = $.extend({}, allBindings.get("options") || 400, defaultOptions, true);
+            var event = allBindings.get("event") || "click";
+            var actionElement = findActionElement();
+
+            if (intialState)
+                actionElement.slideDown(options);
+            else
+                actionElement.slideUp(options);
+
+            $(el).on(event, function () {
+                actionElement.slideToggle(options)
+            });
+
+            function findActionElement() {
+                var actionElement = $(el);
+                actionElement = parentSelect ? actionElement.closest(parentSelect.startsWith(".") ? parentSelect : "." + parentSelect) : actionElement;
+                actionElement = childSelect ? actionElement.find(childSelect.startsWith(".") ? childSelect : "." + childSelect) : actionElement;
+                return actionElement;
+            };
+        }
+    };
+})();
